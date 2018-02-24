5fc0770fdd2584e9782ee46db840801270de8ab5
jribs <josh.ribeiro3@gmail.com>
2018-02-24 14:28:14 -0500

    Add Google Play Services and implement GoogleAPIClient connection

--- app/src/main/java/com/example/android/shushme/MainActivity.kt
+++ app/src/main/java/com/example/android/shushme/MainActivity.kt
@@ -19,30 +19,63 @@ package com.example.android.shushme
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.places.Places
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+                    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     // Member variables
     private var mAdapter: PlaceListAdapter? = null
+    private lateinit var googleApiClient: GoogleApiClient
 
 
-    /**
-     * Called when the activity is starting
-     *
-     * @param savedInstanceState The Bundle that contains the data supplied in onSaveInstanceState
-     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Set up the recycler view
-        with(recylerview_places_main){
+        makeGoogleAPIClient()
+        setupRecyclerView()
+    }
+
+//GoogleAPI
+override fun onConnected(connectionHint: Bundle?) {
+    Log.d(TAG, "API Connection Successful")
+}
+
+    override fun onConnectionSuspended(p0: Int) {
+        Log.d(TAG, "API Connection Suspended")
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Log.d(TAG, "Connection to Google API Failed")
+    }
+
+
+//2nd Layer Functions
+    private fun setupRecyclerView() {
+        with(recylerview_places_main) {
             layoutManager = LinearLayoutManager(this@MainActivity)
             mAdapter = PlaceListAdapter(this@MainActivity)
             adapter = mAdapter
         }
     }
+
+    private fun makeGoogleAPIClient(){
+        googleApiClient = GoogleApiClient.Builder(this).
+                addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage(this, this)
+                .build()
+    }
+
+
 
     companion object {
         val TAG = MainActivity::class.java.simpleName
